8cf99162641e5bce7af6f915b1b17b25ece02877
jayk <jaykhandkar2002@gmail.com>
2017-05-18 20:18:21 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-if [ $(whereis pcap | wc -m) -lt 7 ]				#pcap not installed
+if [ $(whereis pcap | wc -m) -lt 7 ]					#pcap not installed
 then
 	echo "pcap not found, will try and install"
-	if [ $(whereis apt-get | wc -m) -lt 9 ];then  #check which packet manager
+	if [ $(whereis apt-get | wc -m) -lt 9 ];then  			#check which packet manager
 		sudo yum install libpcap-devel
-		if [ $? != 0 ];then		      #installation failed
+		if [ $? != 0 ];then		      			#installation failed
 			echo "couldn't install required libraries"
 			echo "stop"
 			exit
@@ -17,5 +17,5 @@ then
 		fi
 	fi
 fi
-echo "libraries installed, buiding..."				#pcap found/installed
+echo "libraries installed, buiding..."					#pcap found/installed
 make sniffer
